6818b78a77b76f90204c8b20256f178721590c38
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-04-09 20:35:53 -0400

    Finish filter range exercise

--- javascript_basics/object_basics/arrays.js
+++ javascript_basics/object_basics/arrays.js
@@ -26,3 +26,28 @@ console.log(camelize("background-color"));
 console.log(camelize("list-style-image"));
 console.log(camelize("-webkit-transition-"));
 console.log(camelize("-border-left-width"));
+
+// Filter range exercise
+
+function filterRange(arr, a, b) {
+  // Given arr, look for elements higher or equal to a and lower or equal to b
+
+  // return a new arr don't modify original
+
+  // loop through all elements of arr
+  // test with comparison func
+  // if pass test add to newArr
+  // return newArr
+  function withinRange(num) {
+    return num >= a && num <= b;
+  }
+
+  let newArr = arr.filter(withinRange);
+
+  return newArr;
+}
+
+let arr = [5, 3, 8, 1];
+let filtered = filterRange(arr, 1, 4);
+console.log(arr);
+console.log(filtered);
