24792f618cb7f59745a19f8389b9d0dc02869e1f
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-07-17 19:11:30 -0400

    Update Classes file

---
+++ FullstackJS/JavaScript/Classes/classes.js
@@ -0,0 +1,40 @@
+// "use strict";
+
+// let user = {
+//   name: "John",
+//   surname: "Smith",
+
+//   get fullName() {
+//     return `${this.name} ${this.surname}`;
+//   },
+
+//   set fullName(value) {
+//     [this.name, this.surname] = value.split(" ");
+//   },
+// };
+
+// // now in class syntax
+
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+
+//   sayHi() {
+//     console.log(`Hello ${this.firstName} ${this.lastName}!`);
+//   }
+// }
+
+// const john = new Person("John", "Doe");
+// john.sayHi();
+
+// class Users {
+//   ["say" + "Hi"]() {
+//     console.log("Hello");
+//   }
+// }
+
+// new Users().sayHi();
+
+// Javascript Classes Explained Video Notes
