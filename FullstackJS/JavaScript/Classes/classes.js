c72feec3e76fe2d03e5e0ac81c6ecee171206a0c
Luis Rodriguez <106412395+Luis-Rodriguez-02@users.noreply.github.com>
2025-07-17 19:33:06 -0400

    Testing getters and setters

--- FullstackJS/JavaScript/Classes/classes.js
+++ FullstackJS/JavaScript/Classes/classes.js
@@ -38,3 +38,38 @@
 // new Users().sayHi();
 
 // Javascript Classes Explained Video Notes
+
+class Pizza {
+  constructor(pizzaSize, pizzaType) {
+    this.size = pizzaSize;
+    this.crust = "original";
+    this.pizzaType = pizzaType;
+    this.toppings = [];
+  }
+
+  getCrust() {
+    return this.crust;
+  }
+
+  setCrust(pizzaCrust) {
+    this.crust = pizzaCrust;
+  }
+
+  getToppings() {
+    return this.toppings;
+  }
+
+  setToppings(...toppings) {
+    toppings.forEach((topping) => this.toppings.push(topping));
+  }
+
+  bake() {
+    console.log(
+      `Baking a ${this.size} ${this.pizzaType} ${this.crust} crust pizza.`
+    );
+  }
+}
+
+const myPizza = new Pizza("Small", "Pepperoni");
+myPizza.setToppings("Pepperoni", "Ham", "Salami");
+console.log(myPizza.getToppings());
